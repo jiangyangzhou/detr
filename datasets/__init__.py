ab714018b079ab59ac782e1f5978ce6d282c9aba
jiangyangzhou <1098122556@qq.com>
2022-09-30 18:33:19 +0800

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -14,7 +14,6 @@ def get_coco_api_from_dataset(dataset):
     if isinstance(dataset, torchvision.datasets.CocoDetection):
         return dataset.coco
 
-
 def build_dataset(image_set, args):
     if args.dataset_file == 'coco':
         return build_coco(image_set, args)
@@ -22,4 +21,14 @@ def build_dataset(image_set, args):
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
         return build_coco_panoptic(image_set, args)
+    if args.dataset_file == "gaze_follow"  :
+        transform = _get_transform()
+        gazefollow_train_data = "/mnt/sdb1/gazeFollow/gazefollow_extend"
+        gazefollow_train_label = "/mnt/sdb1/gazeFollow/gazefollow_extend/train_annotations_release.txt"
+        gazefollow_val_data = "/mnt/sdb1/gazeFollow/gazefollow_extend"
+        gazefollow_val_label = "/mnt/sdb1/gazeFollow/gazefollow_extend/test_annotations_release.txt"
+        train_dataset = GazeFollow(gazefollow_train_data, gazefollow_train_label,
+                      transform, input_size=input_resolution, output_size=output_resolution)
+        return train_dataset
     raise ValueError(f'dataset {args.dataset_file} not supported')
+
